42d1cf84cd242305f005b01c9b8ec83b13a35b31
hunger-groomers <demonsstoppie@gmail.com>
2025-05-15 02:36:49 +0530

    Create Review.js

---
+++ models/Review.js
@@ -0,0 +1,65 @@
+// models/Review.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
+  order: {
+    type: Schema.Types.ObjectId,
+    ref: 'Order'
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String,
+    trim: true
+  },
+  foodRating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  serviceRating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  deliveryRating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  images: [String],
+  isApproved: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+reviewSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
